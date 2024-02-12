a6fa9fa50fb89b6f7d7863cec8635db250be2e96
Joyce77777777 <145174906+Joyce77777777@users.noreply.github.com>
2024-02-12 13:37:07 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const express = require('express');
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Import routes
-const htmlRoutes = require('./routes/html-routes');
-const apiRoutes = require('./routes/api-routes');
-
-// Middleware for parsing application/json and urlencoded data
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(api_routes)
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Use routes
-app.use('/', htmlRoutes); // It's a good practice to explicitly define the base route
-app.use('/api', apiRoutes); // Assuming your API routes are prefixed with '/api'
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
